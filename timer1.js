75393937f26423fc2b565568aedbcd4535a77957
Ruta Reisoglu <rutareiso@protonmail.com>
2021-11-30 18:13:15 +0000

    Read command line args and beep at specific seconds

---
+++ timer1.js
@@ -0,0 +1,14 @@
+const timer = (args)=>{
+  for (let arg of args) {
+    const num = Number(arg);
+    if (num > 0) {
+      setTimeout(()=>{
+        process.stdout.write('\x07');
+        console.log("beep at ", arg);
+      }, Number(arg) * 1000);
+    }
+  }
+};
+
+const args = process.argv;
+timer(args.slice(2));
